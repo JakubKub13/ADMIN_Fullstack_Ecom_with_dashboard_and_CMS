87f7866970265b15af9d809c4763a61821c08920
JakubKub13 <jakubkubala3@gmail.com>
2023-08-04 21:28:05 +0200

    Adding store switcher

--- components/store-switcher.tsx
+++ components/store-switcher.tsx
@@ -1,13 +1,14 @@
 "use client";
 
-import { PopoverTrigger, Popover } from "@/components/ui/popover";
+import { PopoverTrigger, Popover, PopoverContent } from "@/components/ui/popover";
 import { Store } from "@prisma/client";
 import { useStoreModal } from "@/hooks/use-store-modal";
 import { useParams, useRouter } from "next/navigation";
 import { useState } from "react";
 import { Button } from "@/components/ui/button";
-import { ChevronsUpDown , Store as StoreIcon } from "lucide-react";
+import { ChevronsUpDown , Store as StoreIcon, Check, PlusCircle } from "lucide-react";
 import { cn } from "@/lib/utils";
+import { Command, CommandList, CommandInput, CommandEmpty, CommandGroup, CommandItem, CommandSeparator } from "@/components/ui/command";
 
 
 
@@ -51,10 +52,52 @@ export default function StoreSwitcher({
             >
                
                 <StoreIcon className="mr-2 h-4 w-4" />
-                Current Store
-                <ChevronsUpDown />
+                {currentStore?.label}
+                <ChevronsUpDown className="ml-auto h-4 w-4 shrink-0 opacity-50" />
             </Button>
         </PopoverTrigger>
+        <PopoverContent className="w-[200px] p-0">
+            <Command>
+                <CommandList>
+                    <CommandInput placeholder="Search store...." />
+                    <CommandEmpty>No store found.....</CommandEmpty>
+                    <CommandGroup heading="Stores">
+                        {formattedItems.map((store) => (
+                            <CommandItem
+                                key={store.value}
+                                onSelect={() => onStoreSelect(store)}
+                                className="text-sm"
+                            >
+                                <StoreIcon className="mr-2 h-4 w-4" />
+                                {store.label}
+                                <Check
+                                    className={cn(
+                                        "ml-auto h-4 w-4",
+                                        currentStore?.value === store.value
+                                        ? "opacity-100"
+                                        : "opacity-0"
+                                    )}
+                                />
+                            </CommandItem>
+                        ))}
+                    </CommandGroup>
+                </CommandList>
+                <CommandSeparator />
+                <CommandList>
+                    <CommandGroup>
+                        <CommandItem
+                            onSelect={() => {
+                                setOpen(false);
+                                storeModal.onOpen();
+                            }}
+                        >
+                            <PlusCircle className="mr-2 h-5 w-5" />
+                            Create new store 
+                        </CommandItem>
+                    </CommandGroup>
+                </CommandList>
+            </Command>
+        </PopoverContent>
        </Popover>
     );
 };
