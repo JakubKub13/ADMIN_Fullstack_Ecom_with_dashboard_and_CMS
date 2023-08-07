d6c58196a49dacc1a68035a7a201d38c8641461e
JakubKub13 <jakubkubala3@gmail.com>
2023-08-07 22:39:06 +0200

    Fixing bug

--- app/(dashboard)/[storeId]/(routes)/products/components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/products/components/client.tsx
@@ -1,46 +1,39 @@
 "use client";
 
-import { Heading } from "@/components/ui/heading";
-import { Button } from "@/components/ui/button";
 import { Plus } from "lucide-react";
-import { Separator } from "@/components/ui/separator";
-import { useRouter, useParams } from "next/navigation";
-import { BillboardColumn, columns } from "./columns";
+import { useParams, useRouter } from "next/navigation";
+
+import { Button } from "@/components/ui/button";
 import { DataTable } from "@/components/ui/data-table";
+import { Heading } from "@/components/ui/heading";
+import { Separator } from "@/components/ui/separator";
 import { ApiList } from "@/components/ui/api-list";
 
-interface BillboardClientProps {
-    data: BillboardColumn[]
-}
+import { ProductColumn, columns } from "./columns";
 
-export const BillboardClient: React.FC<BillboardClientProps> = ({
-    data
+interface ProductsClientProps {
+  data: ProductColumn[];
+};
+
+export const ProductsClient: React.FC<ProductsClientProps> = ({
+  data
 }) => {
-    const router = useRouter();
-    const params = useParams();
+  const params = useParams();
+  const router = useRouter();
 
-
-    return (
-        <>
-            <div className="flex items-center justify-between">
-                <Heading
-                    title={`Billboards (${data.length})`}
-                    description="Manage billboards for your store"
-                />
-                <Button onClick={() => router.push(`/${params.storeId}/billboards/new`)}>
-                    <Plus className="mr-2 h-4 w-4" />
-                    Add New
-                </Button>
-            </div>
-            <Separator />
-            <DataTable
-                searchKey="label"
-                columns={columns}
-                data={data}
-            />
-            <Heading title="API" description="API calls for Billboards"/>
-            <Separator />
-            <ApiList entityName="billboards" entityIdName="billboardId"/>
-        </>
-    )
-}
+  return (
+    <> 
+      <div className="flex items-center justify-between">
+        <Heading title={`Products (${data.length})`} description="Manage products for your store" />
+        <Button onClick={() => router.push(`/${params.storeId}/products/new`)}>
+          <Plus className="mr-2 h-4 w-4" /> Add New
+        </Button>
+      </div>
+      <Separator />
+      <DataTable searchKey="name" columns={columns} data={data} />
+      <Heading title="API" description="API Calls for Products" />
+      <Separator />
+      <ApiList entityName="products" entityIdName="productId" />
+    </>
+  );
+};
