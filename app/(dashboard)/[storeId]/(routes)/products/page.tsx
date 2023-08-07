5ad1d29c21dc88dc36120deb857e37c582838608
JakubKub13 <jakubkubala3@gmail.com>
2023-08-07 19:27:27 +0200

    adding new database models

---
+++ app/(dashboard)/[storeId]/(routes)/products/page.tsx
@@ -0,0 +1,40 @@
+import { BillboardClient } from "./components/client"
+import prismadb from "@/lib/prismadb";
+import { format } from "date-fns";
+//import { BillboardColumn } from "./components/columns";
+
+const ProductsPage = async ({
+    params
+}: {
+    params: { storeId: string }
+}) => {
+    const products = await prismadb.product.findMany({
+        where: {
+            storeId: params.storeId
+        },
+        include: {
+            category: true,
+            size: true,
+            color: true,
+        },
+        orderBy: {
+            createdAt: "desc"
+        }
+    });
+
+    const formattedBillboards: BillboardColumn[] = products.map((item) => ({
+        id: item.id,
+        label: item.label,
+        createdAt: format(item.createdAt, "MMMM do, yyyy")
+    }))
+
+    return (
+        <div className="flex-col">
+            <div className="flex-1 space-y-4 p-8 pt-6">
+                <BillboardClient data={formattedBillboards}/>
+            </div>
+        </div>
+    )
+}
+
+export default ProductsPage
